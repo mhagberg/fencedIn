76469a1cc95804f19dee993ddfe9c8d2a34950fe
Mike Hagberg <mike.hagberg@experticity.com>
2015-10-12 22:57:37 -0600

    fixed checkout time bug when no checkin had occurred.

--- client/utils/checkInOut.js
+++ client/utils/checkInOut.js
@@ -2,10 +2,10 @@
 UI.registerHelper("showCheckOutOnLastCheckIn", function(jobId) {
   var showCheckout =false;
   var checkIn = JobCheckIns.findOne({job_id: jobId}, {sort:{checkOutTime: 1}},{limit: 1});
-  if (checkIn.checkOutTime === null){
+  if (checkIn && checkIn.checkOutTime === null){
     showCheckout = true;
   }
-  return showCheckout;
+  return showCheckout
 });
 
 UI.registerHelper("hasImage", function(image) {
