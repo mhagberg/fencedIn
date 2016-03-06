54db23f698fb07e8df6b4adf50c983930f488294
Mike Hagberg <mike.hagberg@experticity.com>
2016-03-06 00:28:46 -0700

    added search
    fixed filter by foreman

--- backup-mongodb.sh
+++ backup-mongodb.sh
@@ -9,6 +9,7 @@
 
 read mongo_auth
 
+echo "hi"
 db_name=`echo $mongo_auth | awk '{split($0,array,"/")} END{print array[4]}'`
 ar=`echo $mongo_auth | tr '//' '\n' | grep client | tr ':' '\n' | head -n 2 | tr '@' '\n' | tr '\n' ':'`
 
@@ -22,3 +23,6 @@ host=`echo $ar | awk '{split($0,array,":")} END{print array[3]}'`
 # echo $db_name
 
 mongodump -h $host --port 27017 --username $username --password $password -d $db_name
+
+
+#mongodump -h ds023478.mlab.com:23478 -d fencedin -u mhagberg -p j6yb1rd -o /Users/mhagberg/meteor/projects/fencedIn/dump/fencedin_meteor_com
---
+++ deployToGalaxy.sh
@@ -0,0 +1,1 @@
+DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy fencedin.secomafence.com --settings /Users/mhagberg/meteor/projects/fencedIn/settings.json
