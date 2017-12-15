398bb64287cdc6594ead4451a984257a94f621cf
Mike Hagberg <mike.hagberg@gmail.com>
2017-12-14 22:55:17 -0700

    changed job load back to 1000

--- imports/api/jobs.js
+++ imports/api/jobs.js
@@ -14,17 +14,17 @@ if (Meteor.isServer) {
         return jobs;
     });
     Meteor.publish('jobs', function jobsPublication() {
-        let jobs = Jobs.find({$and:[{hidden : null}, {$or:[{'finishDate': null}, {'finishDate': ""}]}]}, {sort : {createDate : -1}, limit:3000}, {createDate : 1, name : 1, number: 1});
+        let jobs = Jobs.find({$and:[{hidden : null}, {$or:[{'finishDate': null}, {'finishDate': ""}]}]}, {sort : {createDate : -1}, limit:1000}, {createDate : 1, name : 1, number: 1});
         return jobs;
     });
 
     Meteor.publish('hiddenJobs', function hiddenJobsPublication() {
-        let jobs = Jobs.find({'hidden' : true},  {sort:{createDate:-1}, limit:3000}, { name: 1, number: 1, createDate: 1, finishDate: 1});
+        let jobs = Jobs.find({'hidden' : true},  {sort:{createDate:-1}, limit:1000}, { name: 1, number: 1, createDate: 1, finishDate: 1});
         return jobs;
     });
 
     Meteor.publish('job_Reports', function jobReportsPublication() {
-        let jobs = Jobs.find({'finishDate' : {$exists:true, $ne:""}}, {sort:{'finishDate':-1},  limit:3000}, { name: 1, number: 1, createDate: 1, finishDate: 1});
+        let jobs = Jobs.find({'finishDate' : {$exists:true, $ne:""}}, {sort:{'finishDate':-1},  limit:1000}, { name: 1, number: 1, createDate: 1, finishDate: 1});
         return jobs;
     });
     Meteor.publish('allJobs', function allJobsPublication() {
