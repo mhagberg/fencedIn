8c6aab491ff8ce0183b1cb7e56b5a59712b3abc5
Mike Hagberg <mike.hagberg@gmail.com>
2019-04-29 18:32:48 -0600

    Added a calculator for hours to the details page.

--- client/admin/jobReport.js
+++ client/admin/jobReport.js
@@ -1,46 +1,6 @@
 /**
  * Created by mike.hagberg on 11/25/16.
  */
-Template.jobReport.helpers({
-    totalCheckIns : function(jobId) {
-        var checkInCount = JobCheckIns.find({job_id: jobId}).count();
-        return checkInCount;
-    },
-    totalHoursLogged : function (jobId) {
-        var totalHours =0;
-        var checkIns = JobCheckIns.find({job_id: jobId});
-        checkIns.forEach(function(checkin) {
-            var startTime = moment(checkin.checkInTime);
-            var endTime = moment(checkin.checkOutTime);
-            var duration = moment.duration(endTime.diff(startTime));
-            var hours = duration.asHours();
-            totalHours = hours + totalHours;
-        });
-        return totalHours;
-    },
-
-    totalManHours : function (jobId) {
-        var totalManHours =0;
-        var numberOfWorkers =1;
-        var checkIns = JobCheckIns.find({job_id: jobId});
-        checkIns.forEach(function(checkin) {
-            var startTime = moment(checkin.checkInTime);
-            var endTime = moment(checkin.checkOutTime);
-            var duration = moment.duration(endTime.diff(startTime));
-            var hours = duration.asHours();
-            if (checkin.foremen) {
-                numberOfWorkers = checkin.foremen.length;
-            }
-            if (checkin.fencers)
-            {
-                numberOfWorkers = checkin.fencers.length + numberOfWorkers;
-            }
-
-            totalManHours = (hours * numberOfWorkers) + totalManHours;
-        });
-        return totalManHours;
-    }
-});
 Template.jobReport.events({
 'click .img-responsive' : function(e) {
     e.preventDefault();
--- client/utils/job.js
+++ client/utils/job.js
@@ -3,3 +3,42 @@ UI.registerHelper('loadTimePlusTravelTime', function (checkInId) {
     return Number(checkIn.loadTime) + Number(checkIn.travelTime);
 
 });
+
+UI.registerHelper('totalCheckIns', function (jobId) {
+    var checkInCount = JobCheckIns.find({job_id: jobId}).count();
+    return checkInCount;
+});
+
+UI.registerHelper('totalHoursLogged', function (jobId) {
+    var totalHours = 0;
+    var checkIns = JobCheckIns.find({job_id: jobId});
+    checkIns.forEach(function (checkin) {
+        var startTime = moment(checkin.checkInTime);
+        var endTime = moment(checkin.checkOutTime);
+        var duration = moment.duration(endTime.diff(startTime));
+        var hours = duration.asHours();
+        totalHours = hours + totalHours;
+    });
+    return totalHours;
+});
+
+UI.registerHelper('totalManHours', function (jobId) {
+    var totalManHours = 0;
+    var numberOfWorkers = 1;
+    var checkIns = JobCheckIns.find({job_id: jobId});
+    checkIns.forEach(function (checkin) {
+        var startTime = moment(checkin.checkInTime);
+        var endTime = moment(checkin.checkOutTime);
+        var duration = moment.duration(endTime.diff(startTime));
+        var hours = duration.asHours();
+        if (checkin.foremen) {
+            numberOfWorkers = checkin.foremen.length;
+        }
+        if (checkin.fencers) {
+            numberOfWorkers = checkin.fencers.length + numberOfWorkers;
+        }
+
+        totalManHours = (hours * numberOfWorkers) + totalManHours;
+    });
+    return totalManHours;
+});
