eadfb3b3cf01efc3677c5ba4128bc928e4ff898d
mhagberg <mhagberg@sofi.org>
2020-03-18 23:18:52 -0600

    Update to Meteor and all the libraries
    Hide the finish button for mobile.

--- backup-mongodb.sh
+++ backup-mongodb.sh
@@ -1,1 +1,1 @@
-mongodump -h ds131964-a0.mlab.com:31964 -d fencedin_prod -u mhagberg -o /Users/doug/meteor/projects/fencedIn/dump/fencedin_meteor_com
+mongodump -h ds131964-a0.mlab.com:31964 -d fencedin_prod -u mhagberg -o /Users/mhagberg/IdeaProjects/fencedIn/dump/fencedin_meteor_com
--- server/restore.sh
+++ server/restore.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
- mongorestore -h 127.0.0.1 --port 3001 -d meteor /Users/doug/meteor/projects/fencedIn/dump/fencedin_meteor_com/fencedin_prod
+ mongorestore -h 127.0.0.1 --port 3001 -d meteor /Users/mhagberg/IdeaProjects/fencedIn/dump/fencedin_meteor_com/fencedin_prod
