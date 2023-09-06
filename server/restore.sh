a21a6f124e7d42cc564f44030c48d2aed0c7b1c0
mike <mike.hagberg@gmail.com>
2023-09-06 12:43:52 -0600

    adding updates

--- server/restore.sh
+++ server/restore.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
- mongorestore -h 127.0.0.1 --port 3001 -d meteor /Users/mike/IdeaProjects/fencedIn/dump/fencedin_meteor_com/fencedin_prod
+ mongorestore -h 127.0.0.1 --port 3001 -d meteor /Users/mike/IdeaProjects/fencedIn/dump/archived/8-23-2016/fencedIn
